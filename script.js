b9c36b678c06df7fffff2b5a90b26932a0e8b8e0
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-07-28 15:54:06 +0200

    all devises active states finish

--- script.js
+++ script.js
@@ -3,24 +3,18 @@ const submitBtn = document.getElementById("button-submit");
 const singUpCard = document.getElementById("card-signup");
 const successCard = document.getElementById("card-success");
 const errorMessage = document.getElementById("error-message");
+const successMessage = document.getElementById("success-message");
 
-submitBtn.addEventListener("click", () => {
+submitBtn.addEventListener("submit", () => {
   const inputValue = emailInput.value;
 
-  console.log(inputValue);
-
   if (inputValue.includes("@gmail.com")) {
-    console.log("great");
     singUpCard.classList.toggle("hidden");
     successCard.classList.toggle("hidden");
-    // adds class success
-
+    successMessage.textContent = inputValue;
   } else {
     console.log("error");
     errorMessage.classList.toggle("hidden");
-    emailInput.classList.toggle("error")
-
-
-    //adds class error
+    emailInput.classList.toggle("error");
   }
 });
